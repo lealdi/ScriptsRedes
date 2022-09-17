3cdfdd0d72acea93944a1d7886e085bb07958a09
leal <diegoleal1913@gmail.com>
2022-09-17 00:17:24 -0300

    Alterações

--- portscan.py
+++ portscan.py
@@ -1,1 +1,15 @@
-print('Hello World')
+import socket
+
+host = input('Digite o host: ')
+porta = int(input('Digite o número da porta: '))
+
+def scannear():
+    varredor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    verificador = varredor.connect_ex((host,porta))
+    if verificador == 0:
+        print(f'[+] Porta {porta} -> ABERTA ')
+    else:
+        print(f'[+] Porta {porta} -> FECHADA ')
+
+scannear()
+
